2810348a142ed9fd553aa9069fcbef5a121c1b2d
Iman Dezfuly <Iman_Dezfuly@racv.com.au>
2020-08-18 12:28:14 +1000

    initial commit

--- src/server/handlers/handlers.spec.js
+++ src/server/handlers/handlers.spec.js
@@ -3,15 +3,16 @@ import * as Helpers from './helpers';
 import Houses, { INITIAL_DATA } from '../in-memory-db';
 
 describe('request handlers', () => {
-	const mockGetReq = { query: { suburb: 'Point Cook' } };
 	const mockRes = { send: jest.fn() };
 	Helpers.addAverageFlag = jest.fn(() => 'addAverageFlagResult');
-	Houses.init(INITIAL_DATA);
+
+	beforeEach(() => Houses.init(INITIAL_DATA));
 
 	describe('getHandler', () => {
 		beforeEach(() => jest.clearAllMocks());
+		const mockGetReq = { query: { suburb: 'Point Cook' } };
 
-		it('calls the addAverageFlag', () => {
+		it('calls the addAverageFlag with the results from DB', () => {
 			getHandler(mockGetReq, mockRes);
 			expect(Helpers.addAverageFlag).toHaveBeenCalledWith([
 				{
@@ -24,6 +25,40 @@ describe('request handlers', () => {
 					zip: '3030',
 				},
 			]);
+		});
+
+		it('calls the res with the response from addAverageFlag', () => {
+			getHandler(mockGetReq, mockRes);
+			expect(mockRes.send).toHaveBeenCalledWith('addAverageFlagResult');
+		});
+	});
+
+	describe('postHandler', () => {
+		beforeEach(() => jest.clearAllMocks());
+
+		it('creates a new record in DB for valid data', () => {
+			const mockPostRequest = {
+				body: { suburb: 'Point Cook', description: ' new house' },
+			};
+			postHandler(mockPostRequest, mockRes);
+			expect(Houses.findAllBySuburb('Point Cook').length).toBe(2);
+		});
+
+		it('throws error if suburb is empty', () => {
+			const mockPostRequest = {
+				body: { suburb: '', description: ' new house' },
+			};
+			let error = null;
+			try {
+				postHandler(mockPostRequest, mockRes);
+			} catch (err) {
+				error = err;
+			}
+			expect(Houses.findAllBySuburb('Point Cook').length).toBe(1);
+			expect(error).toEqual({
+				code: 501,
+				message: 'cannot insert house without suburb',
+			});
 		});
 	});
 });
